1b9309350c32fd618db05cde4ab99f883bd03c69
Ryoju Ohata <ryozu256@gmail.com>
2021-09-09 16:35:46 +0900

    Add yen mark #14

--- app/src/pages/payment.tsx
+++ app/src/pages/payment.tsx
@@ -140,16 +140,16 @@ export default function Home() {
                       value={month.price}
                       disabled={month.paid}
                     >
-                      <option value="100">100</option>
-                      <option value="200">200</option>
-                      <option value="300">300</option>
-                      <option value="400">400</option>
-                      <option value="500">500</option>
-                      <option value="600">600</option>
-                      <option value="700">700</option>
-                      <option value="800">800</option>
-                      <option value="900">900</option>
-                      <option value="1000">1,000</option>
+                      <option value="100">¥100</option>
+                      <option value="200">¥200</option>
+                      <option value="300">¥300</option>
+                      <option value="400">¥400</option>
+                      <option value="500">¥500</option>
+                      <option value="600">¥600</option>
+                      <option value="700">¥700</option>
+                      <option value="800">¥800</option>
+                      <option value="900">¥900</option>
+                      <option value="1000">¥1,000</option>
                     </select>
                     {/* <input
                   className={utilStyles.input}
